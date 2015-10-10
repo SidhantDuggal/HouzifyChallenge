78bc552d5016db413f3e87d9a23efab03ee2504c
SidhantDuggal <SidhantDuggal@users.noreply.github.com>
2015-10-10 14:13:59 +0530

    3rdCommit:ClickingOnImageOpensFullscreenGallery

--- app/src/main/java/com/houzifychallenge/FullScreenGalleryActivity.java
+++ app/src/main/java/com/houzifychallenge/FullScreenGalleryActivity.java
@@ -1,13 +1,35 @@
 package com.houzifychallenge;
 
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class FullScreenGalleryActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_full_screen_gallery);
+        setContentView(R.layout.activity_fullscreen_gallery);
+
+        String title = getIntent().getStringExtra("title");
+        int position = Integer.valueOf(getIntent().getStringExtra("imagePosition"));
+        int totalImageCount = Integer.valueOf(getIntent().getStringExtra("totalImageCount"));
+        TypedArray imgs = getResources().obtainTypedArray(R.array.image_ids);
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), imgs.getResourceId(position, -1));
+
+        TextView titleTextView = (TextView) findViewById(R.id.fullscreenGallery_textView);
+        titleTextView.setText(title);
+
+        ImageView imageView = (ImageView) findViewById(R.id.fullscreenGallery_imageView);
+        imageView.setImageBitmap(bitmap);
+
     }
+
 }
--- app/src/main/java/com/houzifychallenge/ThumbnailGalleryActivity.java
+++ app/src/main/java/com/houzifychallenge/ThumbnailGalleryActivity.java
@@ -1,19 +1,23 @@
 package com.houzifychallenge;
 
+import android.content.Intent;
 import android.content.res.TypedArray;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.GridView;
 
 import java.util.ArrayList;
 
-public class ThumbnailGalleryActivity extends AppCompatActivity {
+public class ThumbnailGalleryActivity extends AppCompatActivity{
 
     private GridView mGridView;
     private GridViewAdapter mGridAdapter;
+    private int mImageCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,16 +32,33 @@ public class ThumbnailGalleryActivity extends AppCompatActivity {
         mGridAdapter = new GridViewAdapter(this, R.layout.thumbnail_item_layout, getData());
         mGridView.setAdapter(mGridAdapter);
 
+        mGridView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                ThumbnailItem item = (ThumbnailItem) parent.getItemAtPosition(position);
+                Intent intent = new Intent(ThumbnailGalleryActivity.this, FullScreenGalleryActivity.class);
+                intent.putExtra("title", item.getTitle());
+                intent.putExtra("imagePosition", String.valueOf(position));
+                intent.putExtra("totalImageCount", String.valueOf(mImageCount));
+                startActivity(intent);
+
+            }
+
+        });
+
     }
 
     private ArrayList<ThumbnailItem> getData() {
 
         final ArrayList<ThumbnailItem> imageItems = new ArrayList<>();
         TypedArray imgs = getResources().obtainTypedArray(R.array.image_ids);
-        for (int i = 0; i < imgs.length(); i++) {
+        mImageCount = imgs.length();
+        for (int i = 0; i < mImageCount; i++) {
 
             Bitmap bitmap = BitmapFactory.decodeResource(getResources(), imgs.getResourceId(i, -1));
-            imageItems.add(new ThumbnailItem(bitmap, "Image#" + i));
+            imageItems.add(new ThumbnailItem(bitmap, "Title#" + i));
 
         }
         return imageItems;
